b317f2270049b0067354f0ba49bc2da238e03573
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2024-04-13 11:02:09 -0700

    Create and test function operate
    Takes an operator and 2 numbers and then calls one of the
    function (add, subtract, multiply divide) on the numbers

--- javascript.js
+++ javascript.js
@@ -17,3 +17,21 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2;
 };
+
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case("+"):
+        return add(num1, num2);
+
+        case("-"):
+        return subtract(num1, num2);
+
+        case("*"):
+        return multiply(num1, num2);
+
+        case("/"):
+        return divide(num1, num2);
+    }
+}
+
+console.log(operate(2, 4, "/"));
