224fea8a810380f9c944d03bc6e26dbffde5e551
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2024-04-13 16:55:08 -0700

    Add functionality to "operate" function
    Add functions add, subtract, multiply, divide directly to operate().
    Eliminates redundant code.

--- javascript.js
+++ javascript.js
@@ -66,34 +66,18 @@ function clear() {
     return displayValue;
 }
 
-function add (num1, num2) {
-    return Number(num1) + Number(num2);
-};
-
-function subtract (num1, num2) {
-    return num1 - num2;
-};
-
-function multiply (num1, num2) {
-    return num1 * num2;
-};
-
-function divide (num1, num2) {
-    return num1 / num2;
-};
-
 function operate (num1, num2, operator) {
     switch (operator) {
         case("+"):
-        return add(num1, num2);
+        return Number(num1) + Number(num2);
 
         case("-"):
-        return subtract(num1, num2);
+        return num1 - num2;
 
         case("*"):
-        return multiply(num1, num2);
+        return num1 * num2;
 
         case("/"):
-        return divide(num1, num2);
+        return num1 / num2;
     }
 }
